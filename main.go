ba42e0734bcd69473f6319b61339f5a02447a842
Mike Hollister <hollister.michael@gmail.com>
2021-11-14 03:51:35 +0000

    added kebab-case

--- main.go
+++ main.go
@@ -1,16 +1,26 @@
 package main
+
 import (
 	"strings"
 )
-func SnakeCase(words []string) string {
+
+func basecase(words []string, delimiter string) string {
 	var s = ""
 	for i, word := range words {
 		if i > 0 {
-			s += "_"
+			s += delimiter
 		}
 		s += strings.ToLower(word)
 	}
 	return s
+}
+
+func SnakeCase(words []string) string {
+	return basecase(words, "_")
+}
+
+func KebabCase(words []string) string {
+	return basecase(words, "-")
 }
 func main() {
 
--- main_test.go
+++ main_test.go
@@ -14,6 +14,12 @@ func TestSnakeCase(t *testing.T) {
 			t.Error()
 		}
 	}
+	if KebabCase([]string{""}) != "" {
+		t.Error()
+	}
+	if KebabCase([]string{" "}) != " " {
+		t.Error()
+	}
 	if SnakeCase([]string{"astring"}) != "astring" {
 		t.Error()
 	}
@@ -21,6 +27,34 @@ func TestSnakeCase(t *testing.T) {
 		t.Error()
 	}
 	if SnakeCase([]string{"a-String"}) != "a-string" {
+		t.Error()
+	}
+}
+func TestKebabCase(t *testing.T) {
+	correct_strings := [][]string{
+		{"a", "String"},
+		{"A", "String"},
+		{"a", "string"},
+	}
+	for _, test_string := range correct_strings {
+		s := KebabCase(test_string)
+		if s != "a-string" {
+			t.Error()
+		}
+	}
+	if KebabCase([]string{""}) != "" {
+		t.Error()
+	}
+	if KebabCase([]string{" "}) != " " {
+		t.Error()
+	}
+	if KebabCase([]string{"astring"}) != "astring" {
+		t.Error()
+	}
+	if KebabCase([]string{"ASTRING"}) != "astring" {
+		t.Error()
+	}
+	if KebabCase([]string{"a-String"}) != "a-string" {
 		t.Error()
 	}
 }
